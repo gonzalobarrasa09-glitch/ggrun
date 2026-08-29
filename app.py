926870d94c46eb242b20e823bbc090e80ac31fe1
gonzalobarrasa09 <gonzalobarrasa09@gmail.com>
2026-08-29 22:18:28 +0200

    login cutre

--- app.py
+++ app.py
@@ -113,4 +113,10 @@ with gr.Blocks(title="Sports Data Hub") as app:
 if __name__ == "__main__":
     launch_telegram_bot()
     port = int(os.environ.get("PORT", 7860))
-    app.launch(server_name="0.0.0.0", server_port=port, css=custom_css)
+    
+    app.launch(
+        server_name="0.0.0.0", 
+        server_port=port, 
+        css=custom_css, 
+        auth=[("gonzalo", "1234"), ("gay", "1234")]
+    )
